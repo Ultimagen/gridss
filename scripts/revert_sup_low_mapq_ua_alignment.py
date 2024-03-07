c0113ac8893daeca04333618e1a875f6744ba9e2
mayalevy <maya.levy@ultimagen.com>
2024-03-07 09:31:06 +0200

    is_supplementary instead of using flag numbers

--- scripts/revert_sup_low_mapq_ua_alignment.py
+++ scripts/revert_sup_low_mapq_ua_alignment.py
@@ -23,7 +23,7 @@ sup_reads = set()
 
 # reads with supplementary alignment
 for after_read in after.fetch():
-    if after_read.flag & 2048:
+    if after_read.is_supplementary:
         sup_reads.add(after_read.query_name)
 
 # reads that should be reverted:
@@ -31,9 +31,9 @@ for after_read in after.fetch():
 # reads with decoy alignment
 # reads which their primary mapping (flag = 0) quality is low, and they have supplementary alignment read
 for after_read in after.fetch():
-        if (after_read.mapping_quality < args.min_mapping_quality and after_read.flag & 2048) or \
+        if (after_read.mapping_quality < args.min_mapping_quality and after_read.is_supplementary) or \
             (after_read.has_tag('SA') and 'decoy' in after_read.get_tag('SA') or \
-            (after_read.mapping_quality < args.min_mapping_quality and after_read.flag == 0 and after_read.query_name in sup_reads)):
+            (after_read.mapping_quality < args.min_mapping_quality and (not after_read.is_supplementary) and after_read.query_name in sup_reads)):
                 reads_to_remove.add(after_read.query_name)
 
 
